976c16c32f85b2dbcca7c6547dd01cfed89a6d57
MKYRENE <rodriguezm90@icloud.com>
2023-06-09 22:58:12 -0400

    updating html css and js file code with comments

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-// JavaScript code
-// Define the quiz questions and answers
-const questions = [
+
+//VARIABLE FOR QUIZ QUESTIONS 
+var questions = [
     {
       question: "What is the capital of France?",
       choices: ["Paris", "London", "Berlin", "Rome"],
       answer: "Paris"
     },
     {
-      question: "What is the largest planet in our solar system?",
-      choices: ["Jupiter", "Saturn", "Earth", "Neptune"],
-      answer: "Jupiter"
+      question: "Question: Which planet is known as the 'Red Planet' ?",
+      choices: ["Jupiter", "Saturn", "Earth", "Mars"],
+      answer: "Mars"
     },
-    // Add more questions as needed
+    {
+        question: "Who painted the famous artwork 'Mona Lisa'?",
+        choices: ["Vincent Van Gogh", "Leonardo da Vinci", "Pablo Picasso", "Michelangelo"],
+        answer: "Leonardo Da Vinci"
+      },
+      {
+        question: "What is the capital city of Australia?",
+        choices: ["Sydney", "Melbourne", "Canberra", "Brisbane"],
+        answer: "Canberra"
+      },
+      {
+        question: "What is the largest planet in our solar system?",
+        choices: ["Jupiter", "Saturn", "Earth", "Neptune"],
+        answer: "Jupiter"
+      },
   ];
